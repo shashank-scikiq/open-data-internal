3f1d2b75859cb076bf3d7259f936d0fa8927782b
Shashank Raj <shashank.raj@scikiq.com>
2024-07-23 12:25:35 +0530

    Removing ETL from Staging.
    - Removing the ETL Layer from Staging.

--- Init_DB/Final_DB_Scripts/DIM_DISTRICTS_TBL.sql
+++
@@ -1,4 +0,0 @@
-INSERT into POSTGRES_SCHEMA.DIM_DISTRICTS_TBL(delivery_state, delivery_district)
-SELECT distinct "Statename" as delivery_state, "Districtname" as delivery_district
-	FROM POSTGRES_SCHEMA.TBL_PINCODE
-	GROUP BY "Statename",  "Districtname"
