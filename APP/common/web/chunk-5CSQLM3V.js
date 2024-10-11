80c85b4de27a4548a0b9f86c61669b271a3b0c83
AankurScikiq <ankur.pandey@scikiq.com>
2024-10-11 16:17:25 +0530

    updated code with log

--- APP/common/web/chunk-5CSQLM3V.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-A62IFTR7.js";import"./chunk-J6DTJQM2.js";import"./chunk-4J5NWKFU.js";export{a as RetailModule};
--- APP/common/web/chunk-7WT5ZVXX.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-4SBECL4G.js";import"./chunk-J6DTJQM2.js";import"./chunk-4J5NWKFU.js";export{a as LogisticModule};
